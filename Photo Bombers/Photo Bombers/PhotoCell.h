06498e178ce92dca67a1fb733fb901bef483bfb1
Dean Laurea <deanlaurea@icloud.com>
2015-09-02 19:02:11 -0500

    Create PhotoCell class sublcass of UICollectionViewCell

---
+++ Photo Bombers/Photo Bombers/PhotoCell.h
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.h
+//  Photo Bombers
+//
+//  Created by Dean Laurea on 9/2/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCell : UICollectionViewCell
+
+@end
---
+++ Photo Bombers/Photo Bombers/PhotoCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.m
+//  Photo Bombers
+//
+//  Created by Dean Laurea on 9/2/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import "PhotoCell.h"
+
+@implementation PhotoCell
+
+@end
