869a924d11357b62303666adf6d0fef65437ed7a
Dean Laurea <deanlaurea@icloud.com>
2015-09-05 03:44:30 -0500

    Add pod SSKeychain

--- Photo Bombers/Pods/Target Support Files/Pods/Pods-environment.h
+++ Photo Bombers/Pods/Target Support Files/Pods/Pods-environment.h
@@ -48,6 +48,12 @@
 #define COCOAPODS_VERSION_MINOR_SAMCache 3
 #define COCOAPODS_VERSION_PATCH_SAMCache 0
 
+// SSKeychain
+#define COCOAPODS_POD_AVAILABLE_SSKeychain
+#define COCOAPODS_VERSION_MAJOR_SSKeychain 1
+#define COCOAPODS_VERSION_MINOR_SSKeychain 2
+#define COCOAPODS_VERSION_PATCH_SSKeychain 3
+
 // SimpleAuth/Core
 #define COCOAPODS_POD_AVAILABLE_SimpleAuth_Core
 #define COCOAPODS_VERSION_MAJOR_SimpleAuth_Core 0
