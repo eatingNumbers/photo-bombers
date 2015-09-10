3c9b51f7b8178f2783db23a4ea7ac2b775d5c636
Dean Laurea <deanlaurea@icloud.com>
2015-09-09 21:19:16 -0500

    Add snap behavior

--- Photo Bombers/Photo Bombers/DetailViewController.m
+++ Photo Bombers/Photo Bombers/DetailViewController.m
@@ -12,6 +12,7 @@
 @interface DetailViewController ()
 
 @property (nonatomic) UIImageView *imageView;
+@property (nonatomic) UIDynamicAnimator *animator;
 
 @end
 
@@ -22,7 +23,7 @@
 
     self.view.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.95];
     
-    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 320)];
+    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, -320, 320.0f, 320.0f)];
     [self.view addSubview:self.imageView];
     
     [PhotoController imageForPhoto:self.photo size:@"standard_resolution" completion:^(UIImage *image) {
@@ -30,17 +31,21 @@
     }];
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(close)];
     [self.view addGestureRecognizer:tap];
+    
+    self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    
+    
 }
 
-- (void)viewDidLayoutSubviews {
-    [super viewDidLayoutSubviews];
-    CGSize size = self.view.bounds.size;
-    CGSize imageSize = CGSizeMake(size.width, size.width);
-    self.imageView.frame = CGRectMake(0.0, (size.height - imageSize.height)/2.0, imageSize.width, imageSize.height);
+-(void)viewDidAppear:(BOOL)animated {
+    UISnapBehavior *snap = [[UISnapBehavior alloc] initWithItem:self.imageView snapToPoint:self.view.center];
+    [self.animator addBehavior:snap];
 }
-
 
 - (void)close {
+    [self.animator removeAllBehaviors];
+    UISnapBehavior *snap = [[UISnapBehavior alloc] initWithItem:self.imageView snapToPoint:CGPointMake(CGRectGetMidX(self.view.bounds), CGRectGetMaxY(self.view.bounds) + 180.0f)];
+    [self.animator addBehavior:snap];
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
